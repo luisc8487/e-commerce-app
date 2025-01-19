2529088f83a30ad1ff134d906e601cd3e23ab8c3
L C <LF9904@gmail.com>
2025-01-19 13:50:58 -0500

    added express on index file

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hi there!");
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello there!");
+});
+
+app.listen(3000, () => {
+  console.log("Listening");
+});
