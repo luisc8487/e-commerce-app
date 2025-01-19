3ceb7d838dbca411dbeb51853a22073938353e43
L C <LF9904@gmail.com>
2025-01-19 15:53:30 -0500

    added display simple html

--- index.js
+++ index.js
@@ -3,7 +3,16 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello there!");
+  res.send(`
+    <div>
+        <form>
+            <input placeholder="email"/>
+            <input placeholder="password"/>
+            <input placeholder="password confirmation"/>
+            <button>Submit</button/>
+        </form>
+    </div>
+    `);
 });
 
 app.listen(3000, () => {
