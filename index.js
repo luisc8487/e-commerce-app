068492420d154e43d7690be0232e96c71b8bb85c
L C <LF9904@gmail.com>
2025-01-20 16:03:53 -0500

    parsed form data

--- index.js
+++ index.js
@@ -16,8 +16,17 @@ app.get("/", (req, res) => {
 });
 
 app.post("/", (req, res) => {
-    // get the data from the form
-    res.send("Thanks for signing up!");
+  // get the data from the form
+  req.on("data", (data) => {
+    const parsed = data.toString("utf8").split("&");
+    const formData = {};
+    for (let pair of parsed) {
+      const [key, value] = pair.split("=");
+      formData[key] = value;
+    }
+    console.log(formData);
+  });
+  res.send("Thanks for signing up!");
 });
 
 app.listen(3000, () => {
