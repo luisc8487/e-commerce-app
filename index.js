ec5257c4c9810569ce1402223fcce3e5d3aaa53b
L C <LF9904@gmail.com>
2025-01-24 23:13:00 -0500

    added comments about cookie

--- index.js
+++ index.js
@@ -28,9 +28,13 @@ app.post("/", async (req, res) => {
     return res.send("Email in use");
   }
 
-  if(password !== passwordConfirmation) {
+  if (password !== passwordConfirmation) {
     return res.send("Passwords must match");
   }
+
+  // Create the user in our user repo to represent this person
+
+  // Store the id of that user inside the users cookie
 
   res.send("Thanks for signing up!");
 });
