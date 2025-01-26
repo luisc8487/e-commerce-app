ee7ae55607f2936ec6daef96307e219cef8e7785
L C <LF9904@gmail.com>
2025-01-25 23:23:05 -0500

    added a sign in

--- index.js
+++ index.js
@@ -65,7 +65,21 @@ app.get("/signin", (req, res) => {
     `);
 });
 
-app.post("/signin", async(req, res) => {});
+app.post("/signin", async (req, res) => {
+  const {email, password} = req.body;
+
+  const user = await usersRepo.getOneBy(email);
+
+  if (!user) {
+    return res.send("Email not found");
+  } else if (user.password !== password) {
+    return res.send("Incorrect password");
+  }
+
+  req.session.userId = user.id;
+
+  res.send("You are signed in!");
+});
 
 app.listen(3000, () => {
   console.log("Listening");
