2922e3dcf2a992e625875f5082219e12bda0c77c
L C <LF9904@gmail.com>
2025-01-27 13:05:46 -0500

    receiving validation output

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const {check} = require("express-validator");
+const {check, validationResult} = require("express-validator");
 
 const usersRepo = require("../../repositories/users");
 const signupTemplate = require("../../views/admin/auth/signup");
@@ -14,11 +14,14 @@ router.get("/signup", (req, res) => {
 router.post(
   "/signup",
   [
-    check("email").isEmail(),
-    check("password").isLength({min: 5}),
-    check("passwordConfirmation").isLength({min: 5}),
+    check("email").trim().normalizeEmail().isEmail(),
+    check("password").trim().isLength({min: 4, max: 20}),
+    check("passwordConfirmation").trim().isLength({min: 4, max: 20}),
   ],
   async (req, res) => {
+    const errors = validationResult(req);
+    console.log(errors);
+
     const {email, password, passwordConfirmation} = req.body;
 
     const existingUser = await usersRepo.getOneBy({email});
