959dbbf024c11849eada24610dc84d97d4082391
L C <LF9904@gmail.com>
2025-01-27 00:19:45 -0500

    HTML template functions

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -1,20 +1,12 @@
 const express = require("express");
 const usersRepo = require("../../repositories/users");
+const signupTemplate = require("../../views/admin/auth/signup");
+const signinTemplate = require("../../views/admin/auth/signin");
 
 const router = express.Router();
 
 router.get("/signup", (req, res) => {
-  res.send(`
-      <div>
-     Your id is ${req.session.userId}
-          <form method="POST">
-              <input name="email" placeholder="email"/>
-              <input name="password" placeholder="password"/>
-              <input name="passwordConfirmation" placeholder="password confirmation"/>
-              <button>Submit</button/>
-          </form>
-      </div>
-      `);
+  res.send(signupTemplate({req}));
 });
 
 router.post("/signup", async (req, res) => {
@@ -45,15 +37,7 @@ router.get("/signout", (req, res) => {
 });
 
 router.get("/signin", (req, res) => {
-  res.send(`
-       <div>
-          <form method="POST">
-              <input name="email" placeholder="email"/>
-              <input name="password" placeholder="password"/>
-              <button>Sign in</button/>
-          </form>
-        </div>
-      `);
+  res.send(signinTemplate());
 });
 
 router.post("/signin", async (req, res) => {
@@ -78,3 +62,5 @@ router.post("/signin", async (req, res) => {
 
   res.send("You are signed in!");
 });
+
+module.exports = router;
