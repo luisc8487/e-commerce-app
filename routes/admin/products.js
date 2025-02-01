605616a0e7bf8281a5a4aabad6da034cc298fb7e
L C <LF9904@gmail.com>
2025-01-31 20:13:55 -0500

    require authentication

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const multer = require("multer");
 
-const {handleErrors} = require("./middlewares");
+const {handleErrors, requireAuth} = require("./middlewares");
 const ProductsRepo = require("../../repositories/products");
 const productsNewTemplate = require("../../views/admin/products/new");
 const prodcutsIndexTemplate = require("../../views/admin/products/index");
@@ -10,26 +10,30 @@ const {requireTitle, requirePrice} = require("./validators");
 const router = express.Router();
 const upload = multer({storage: multer.memoryStorage()});
 
-router.get("/admin/products", async (req, res) => {
+router.get("/admin/products", requireAuth, async (req, res) => {
   const products = await ProductsRepo.getAll();
   res.send(prodcutsIndexTemplate({products}));
 });
 
-router.get("/admin/products/new", (req, res) => {
+router.get("/admin/products/new", requireAuth, (req, res) => {
   res.send(productsNewTemplate({}));
 });
 
 router.post(
   "/admin/products/new",
+  requireAuth,
   upload.single("image"),
   [requireTitle, requirePrice],
   handleErrors(productsNewTemplate),
   async (req, res) => {
+    if (!req.session.userId) {
+      return res.redirect("/signin");
+    }
     const image = req.file.buffer.toString("base64");
     const {title, price} = req.body;
     await ProductsRepo.create({title, price, image});
 
-    res.redirect('/admin/products');
+    res.redirect("/admin/products");
   }
 );
 
