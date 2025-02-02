0070dc04920d6c3b44a2ec9cb192e45a7503a4d6
L C <LF9904@gmail.com>
2025-02-02 12:46:52 -0500

    added shopping cart boilerplate

---
+++ routes/carts.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const CartsRepo = require("../repositories/carts");
+const router = express.Router();
+
+router.post("/", async (req, res) => {});
+
+router.get("/", async (req, res) => {});
+
+router.post("/", async (req, res) => {});
+
+module.exports = router;
